d375e9f1c2de01e96d1e2840fad1e087418f0b70
srcrs <srcrs@foxmail.com>
2020-10-06 13:03:28 +0800

    更改每个视频错误肚子承担，互补影响

--- bilibili/src/main/java/org/example/App.java
+++ bilibili/src/main/java/org/example/App.java
@@ -16,20 +16,24 @@ public class App
         ChromeOptions chromeOptions=new ChromeOptions();
         chromeOptions.setHeadless(Boolean.TRUE);
         System.setProperty("webdriver.chrome.driver","chromedriver");
-		chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         Random random = new Random();
         int n=3;
         while(n--!=0){
             if(!("000".equals(args[n]))){
                 WebDriver driver = new ChromeDriver(chromeOptions);
-                Thread.sleep(2000);
+                Thread.sleep(5000);
                 driver.get(args[n]);
-                Thread.sleep(3000);
-                driver.findElement(By.xpath("//*[@id=\"bilibiliPlayer\"]/div[1]/div[1]/div[10]/div[2]/div[2]/div[1]/div[1]/button[1]")).click();
+                Thread.sleep(5000);
+                try{
+                    driver.findElement(By.xpath("//*[@id=\"bilibiliPlayer\"]/div[1]/div[1]/div[10]/div[2]/div[2]/div[1]/div[1]/button[1]")).click();
+                    System.out.println(n+"------------->播放成功");
+                } catch(Exception e){
+                    System.out.println(n+"------------->播放失败\n"+e);
+                }
                 //观看视频时间随机。
                 Thread.sleep((20+random.nextInt(50))*1000);
+                Thread.sleep(20000);
                 driver.close();
-			    Thread.sleep(20000);
             }
         }
     }
